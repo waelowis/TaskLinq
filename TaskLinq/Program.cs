b74026299ca03df30572c9df550aaf8411bb63c1
wael <163440911+waelowis@users.noreply.github.com>
2025-06-10 20:33:01 +0300

    10

--- TaskLinq/Program.cs
+++ TaskLinq/Program.cs
@@ -7,6 +7,7 @@ namespace TaskLinq
         static void Main(string[] args)
         {
             ApplicationDBContext db = new ApplicationDBContext();
+            //1-List all customers' first and last names along with their email addresses.
 
 
         }
